27212fd138c5af42cb0cf681cde5b9aa74af736c
Gustav Nikolaj Olsen <gno@one.com>
2016-06-28 16:49:56 +0200

    add systemjs-builder nested scripttag test case

--- test/systemjs-builder.spec.js
+++ test/systemjs-builder.spec.js
@@ -31,4 +31,43 @@ describe('systemjs-builder', function () {
             });
         });
     });
+    it('should register a nested template', function () {
+        // It will pick up a nested module as a single module, and thus it is
+        // up to assetgraph and the inlined injectTemplates method to split the
+        // templates into separate html tags. Unless the fetch hook can fan out
+        // into multiple loaded modules, I cannot see how we can fix this.
+        var testCasePath = path.resolve(__dirname, '../fixtures/nested');
+        var configPath = path.resolve(testCasePath, 'config.js');
+        var builder = new Builder();
+        builder.config({ baseURL: testCasePath });
+        builder.loadConfigSync(configPath);
+        return expect(builder.bundle('main.js'), 'to be fulfilled').then(function (bundle) {
+            return expect(bundle, 'to satisfy', {
+                modules: [
+                    'main.js',
+                    'template.ko!tpl.js'
+                ],
+                tree: {
+                    'template.ko!tpl.js': {
+                        metadata: {
+                            templateContent: [
+                                '<script type="text/html" id="nestedTemplateOne">',
+                                '    <h1>NESTED TEMPLATE ONE</h1>',
+                                '</script>',
+                                '<script type="text/html" id="nestedTemplateTwo">',
+                                '    <h1>NESTED TEMPLATE TWO</h1>',
+                                '</script>'
+                            ].join('\n') + '\n'
+                        }
+                    }
+                },
+                assetList: [
+                    {
+                        url: expect.it('to match', /template.ko/),
+                        type: 'knockout-template'
+                    }
+                ]
+            });
+        });
+    });
 });
